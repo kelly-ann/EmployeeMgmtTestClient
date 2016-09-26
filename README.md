2ea771de9bcfb3254ae165873ae5c4bfd54be15e
Kelly-Ann <kapeart@gmail.com>
2016-09-26 14:42:26 -0400

    Final project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EmployeeMgmtTestClient
 A Java EE web app client using stateless EJB 3's and WildFly/JBoss app server.
+
+NOTE!  This project only works with WildFly 10.0.0 NOT WildFly 10.1.0!
+
--- src/main/java/com/kelly_ann/employeemgmt/Main.java
+++ src/main/java/com/kelly_ann/employeemgmt/Main.java
@@ -9,6 +9,7 @@ import javax.naming.NamingException;
 
 import com.kelly_ann.employeemgmt.service.EmployeeMgmtService;
 import com.kelly_ann.employeemgmt.domain.Employee;
+import com.kelly_ann.employeemgmt.exception.ServiceUnavailableException;
 
 
 public class Main {
@@ -34,10 +35,31 @@ public class Main {
 			
 			// lookup() format is: jarName/implClassName!interfaceFullyQualifiedClassName
 			EmployeeMgmtService service = (EmployeeMgmtService)jndi.lookup("emsa/EmployeeMgmtImpl!com.kelly_ann.employeemgmt.service.EmployeeMgmtService");
-			List<Employee> employees = service.getAllEmployees();
+			
+			// add new employees
+			Employee e1 = new Employee("Hillary", "Clinton", "Politician", 500);
+			Employee e2 = new Employee("Donald", "Trump", "Businessman", 150);
+			Employee e3 = new Employee("Kelly", "Ann", "Software Engineer", 88);
+			try {
+				service.registerEmployee(e1);
+				service.registerEmployee(e2);
+				service.registerEmployee(e3);
+			} catch (ServiceUnavailableException e) {
+				System.out.println("ServiceUnavailableException thrown: ........ Something went wrong: ........");
+				//e.printStackTrace();
+			}
+			
+			
+			// search for employee(s)
+			List<Employee> employees = service.searchByLastName("Ann");
 			for(Employee employee : employees) {
 				System.out.println(employee);
 			}
+			
+			// call the method that doesn't use any of the Java EE transactional api
+			System.out.println("\nThis call to dummyInMemoryNoTransactionNeededMethod() with return of '" 
+					+ service.dummyInMemoryNoTransactionNeededMethod() + "' did not use Java EE transactions." );
+			
 		}
 		catch (NamingException ne) {
 			ne.printStackTrace();
